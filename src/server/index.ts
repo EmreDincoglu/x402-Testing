ca0fc21afb7ee4dc32213df37c53b9359ee31384
Emre Dincoglu <Dincoglue@gmail.com>
2026-04-07 16:33:45 -0400

    Bazaar Entries now saved in database
    BazaarEntries class will read and write to database to keep persistent entries when relaunching, it reads the DB at construction and loads all info into a mapping which is used in deployment (I may end up removing it later since it may be irrelevant)
    
    Also fixed mispelling with a json object for the seller, this caused the bazaar to not recognize it as a bazaar entry and never stored it.
    
    Changed the layout of the server ts file to imo reduce the amount of bloat being shoved into a single function
    
    Made the client file loop and buy two different resources instead of just one, it makes it easier to demo and show its ability to buy info and show bazaar entries

--- src/server/index.ts
+++ src/server/index.ts
@@ -11,8 +11,8 @@ const app = express();
 const evmAddress = process.env.SERVER_PUBLIC_KEY;
 
 if (evmAddress == undefined) {
-    console.log("Server public key must be provided")
-    process.exit(1)
+  console.log("Server public key must be provided")
+  process.exit(1)
 }
 
 // Create facilitator client (testnet)
@@ -23,42 +23,57 @@ const facilitatorClient = new HTTPFacilitatorClient({
 
 // Registers different servers, only ones we will care abt are of course Arb Sepolia and Arb One
 const resourceServer = new x402ResourceServer(facilitatorClient)
-      .register("eip155:421614", new ExactEvmScheme())
+  .register("eip155:421614", new ExactEvmScheme())
+
+const paymentOptions = {
+  // Schemes are for types of payments, Exact and upto are the main ones (exactly .01 dollars or upto .01 dollars)
+  scheme: "exact", 
+  // Price can be in USD if default asset of the chain is supplied, it is on ArbOne and ArbSep
+  price: "$0.001", 
+  network: "eip155:421614",
+  payTo: evmAddress,
+}
+
+const routes = {
+  "GET /weather": {
+    accepts: paymentOptions,
+    description: "Todays weather forecast",
+    mimeType: "application/json",
+    extensions: {
+      // This is for bazaar visibility, it adds extra info to what the endpoint returns
+      // and is saved in the bazaar, this is NOT shown via the normal endpoint in this case
+      // http://localhost:4021/weather
+      ...declareDiscoveryExtension({
+        //@ts-expect-error (method works but shows err for some reason)
+        method: "GET",
+        output: {
+          example: {
+            weather: "Rain", temperature: -40
+          }
+        }
+      })
+    },
+  },
+  "GET /date": {
+    accepts: paymentOptions,
+    description: "Gives the Date",
+    mimeType: "application/json",
+    extensions: {
+      ...declareDiscoveryExtension({
+        //@ts-expect-error
+        method: "GET",
+        output: {
+          example: { date: "11/23/2001", }
+        }
+      })
+    },
+  },
+}
 
 app.use(
   paymentMiddleware(
-    {
-      "GET /weather": {
-        accepts: 
-          {
-            // There are two major schemes ive seen which are "exact" and "upto"
-            // Exact means the server wants exactly the price shown, and upto means
-            // They want upto the price shown.
-            scheme: "exact",
-            // Price is only able to be shown in dollar format if the network your on has a default stablecoin
-            price: "$0.001",
-            network: "eip155:421614", // Arb Sepolia
-            payTo: evmAddress,
-          },
-          extensions: {
-
-            // This is for bazaar visibility, it adds extra info to what the endpoint returns
-            // and is saved in the bazaar, this is NOT shown via the normal endpoint in this case
-            // http://localhost:4021/weather
-            ...declareDiscoveryExtension({
-              //@ts-expect-error (method works but shows err for some reason)
-              method: "GET",
-              output: {
-                example: {
-                  weather: "Rain", temperature: -40
-                }
-              }
-            })
-          },
-        description: "Todays weather forecast",
-        mimeType: "application/json",
-      },
-    },
+    //@ts-expect-error
+    routes,
     resourceServer
   ),
 );
@@ -73,6 +88,12 @@ app.get("/weather", (req, res) => {
     },
   });
 });
+
+app.get("/date", (req, res) => {
+  res.send({
+    date: "its today lol"
+  })
+})
 
 app.listen(4021, () => {
   console.log(`Server listening at http://localhost:4021`);
